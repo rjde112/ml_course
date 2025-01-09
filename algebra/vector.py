ce50e9362f7b31f1ef73bc9d80cd7a4aa81c9918
gphoenix14 <104388188+gphoenix14@users.noreply.github.com>
2025-01-09 11:37:48 +0100

    updated numpy script

--- algebra/vector.py
+++ algebra/vector.py
@@ -1,8 +1,31 @@
 import numpy as np 
 
-A = np.array([[1,2,3],[4,5,6],[7,8,9]]) #This is a matrix
-B = np.array ([10,11,12]) #this is a vector
-C = np.array ([13,14,15])
-dot_product = np.dot(B,C)
+#Array definition
+u = np.array([3,4,5])
+v = np.array([1,2,3])
 
-print ("The dot product is: ", dot_product)
+#Vector sum
+sum_vector = u + v
+print("Sum (u - v):", sum_vector )
+
+#Difference
+diff_vectors = u - v
+print("Difference (u - v):", diff_vectors)
+
+#dot product
+dot_product = np.dot(u,v)
+print("Dot product:" , dot_product)
+
+#Vector Product (cross product)
+cross_product = np.cross(u,v)
+print ("Cross product (u x v)", cross_product)
+
+#vector norm
+norm_u = np.linalg.norm(u)
+print("norm of (|u|):", norm_u)
+
+#vector normalization
+normalized_u = u / norm_u
+print("Normalized vector u:", normalized_u)
+
+
